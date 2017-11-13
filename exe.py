03a038eecc644b30cbdbd098714f844c9fc08cc2
dotieuthien <30580619+dotieuthien@users.noreply.github.com>
2017-11-13 12:32:35 +0700

    T

--- exe.py
+++ exe.py
@@ -2,25 +2,32 @@
 Author: Do Tieu Thien
 Date: 06/11/1017
 """
-"""Import"""
+
+""" IMPORT """
 import cv2
 import numpy as np
 from fnc.Fire_detection.Segmentation import Segmentation
 from fnc.Fire_detection.Removal import Removal
-
-""" Init """
-image = cv2.imread('Image/5.png')
+import time
+start = time.time()
+""" INIT """
+image = cv2.imread('Image/3.png')
 image = np.double(image) # Double type for Calculation
-image_consecutive = cv2.imread('Image/6.png')
+image_consecutive = cv2.imread('Image/4.png')
 image_Consecutive = np.double(image_consecutive)
 
-""" FIRE DETECTION"""
-
-""" Segmentation flames for first image """
+""" FIRE DETECTION """
+""" Segmentation flames for image """
 image_1 = Segmentation(image) # Type of image_1 is double
 image_2 = Segmentation(image_consecutive)
 
 """ Removal spurious flames """
 image_flames = Removal(image_1,image_2)
+print(time.time() - start)
+img = np.uint8(image_flames)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 """ SMOKE DETECTION """
+""" Segmentation smoke region """
--- fnc/Fire_detection/Removal.py
+++ fnc/Fire_detection/Removal.py
@@ -12,35 +12,12 @@ def Removal(image1, image2):
     im1 = (image1[:,:,2] == 0)
     im1 = im1*255
     image1[:,:,0] = image1[:,:,0] + im1[:,:]
-
-    img1 = np.uint8(image1)
-    cv2.imshow('image', img1)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
     # Convert background of image 2 into blue
     im2 = (image2[:,:,2] == 0)
     im2 = im2*255
     image2[:,:,0] = image2[:,:,0] + im2[:,:]
-
-    img2 = np.uint8(image2)
-    cv2.imshow('image', img2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
     # Remove spurious flames
     image_sub = np.abs(image1 - image2)
-
-    im1 = np.uint8(image_sub)
-    cv2.imshow('image', im1)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
     image_sub = Pos_Segmentation(image_sub)
-
-    im = np.uint8(image_sub)
-    cv2.imshow('image', im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
     return(image_sub)
